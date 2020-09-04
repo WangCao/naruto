eb08d0b7d3e7fc060edaa99d546023fc8bc3be45
WangCao <345619747@qq.com>
2020-09-04 21:18:41 +0800

    feat: add navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,38 @@
 import React from "react";
-import { Box, Flex, Link } from "@chakra-ui/core";
+import { Box, Flex, Link, Button } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
 
 interface Props {}
 const NavBar: React.FC<Props> = () => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>登录</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>注册</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button color="#1a202c" variant="link">
+          退出登录
+        </Button>
+      </Flex>
+    );
+  }
   return (
     <Flex bg="tomato" p={4} ml="auto">
-      <Box ml="auto">
-        <Link color="white" mr={2}>
-          登录
-        </Link>
-        <Link color="white">注册</Link>
-      </Box>
+      <Box ml="auto">{body}</Box>
     </Flex>
   );
 };
